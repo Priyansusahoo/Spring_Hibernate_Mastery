7dcb8fbd273fabd2289fd943a346e73f9e77ad51
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-04-05 23:20:36 +0530

    Init 05-command-line-demo

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/priyansu/application/bluecomet/controller/TestController.java
@@ -0,0 +1,40 @@
+package com.priyansu.application.bluecomet.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * {@code @Author} Priyansu Sahoo
+ */
+/*
+ * NOTE 1:
+ * Here @Controller + @ResponseBody = @RestController
+ * This annotation is a specialization of @Controller.
+ * It is used to create REST-ful web services that return JSON/XML responses directly to the client
+ * instead of rendering a view.
+ * Essentially, @RestController combines @Controller and @ResponseBody,
+ * where every method implicitly serializes return objects into
+ * JSON/XML and sends it as an HTTP response.
+ *
+ * We can also use @Controller for the CLASS and for that specific METHOD we can use @ResponseBody.
+ */
+@Controller // refer to NOTE 1.
+public class TestController {
+
+    /**
+     * NOTE 2:
+     * "TestController.name" is defined in "application.properties" and by
+     * using @Value springboot injects the value of "TestController.name" i.e. "Priyansu" to field "name".
+     */
+    @Value("${TestController.name}") // refer to NOTE 2.
+    private String name;
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test(){
+        return this.name + " test"; // Here we return the "name" field injected by springboot from "application.properties"
+    }
+}
